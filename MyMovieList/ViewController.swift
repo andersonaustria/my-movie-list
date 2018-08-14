07e02da3c1cda06f576d53eea0cf2c24c1ec68db
Anderson Austria <anderson@andersons-macbook-pro.local>
2018-08-14 17:19:47 +0800

    Initial Commit

---
+++ MyMovieList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovieList
+//
+//  Created by Anderson Austria on 14/08/2018.
+//  Copyright © 2018 Anderson Austria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
